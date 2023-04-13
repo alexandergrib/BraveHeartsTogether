02f43ec0300b2cc8e11aa23beea06c76b378b9a5
Aria-vero-s <ariane.saulnier@gmail.com>
2023-04-13 21:01:44 +0000

    Created profile model

---
+++ accounts/forms.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+from django import forms
+from django.db import models
+from .models import Profile
+
+
+class RegisterUserForm(UserCreationForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterUserForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['First_name', 'Last_name', 'Email']
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,72 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+
+from .forms import RegisterUserForm, ProfileForm
+from .models import Profile
+from django.urls import path
+from . import views
+
+# login / logout / user registration system
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.success(request, "There was an error logging in, please try again")
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
 
 
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You were logged out")
+    return redirect('login')
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Resigstration successful!"))
+            return redirect('profile')
+    else:
+        form = RegisterUserForm()
+    return render(request, 'register_user.html', {'form': form, })
+
+
+# form inputs
+def First_name(request):
+    if request.method == "POST":
+        First_name = request.POST["First_name"]
+
+
+def Last_name(request):
+    if request.method == "POST":
+        Last_name = request.POST["Last_name"]
+
+
+def Email(request):
+    if request.method == "POST":
+        Email = request.POST["Email"]
+
+
+def account(request):
+    profiles = Profile.objects.filter(user=request.user)
+    context = {
+        'profiles': profiles
+    }
+    return render(request, 'templates/account.html', context)
