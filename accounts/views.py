30f847d4fb8b975ef1169039b8b41909b2f5ccbb
Alex <pioneerko@hotmail.com>
2023-04-14 17:20:29 +0100

    Fixed login functionality csrf error

--- accounts/views.py
+++ accounts/views.py
@@ -17,7 +17,7 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('index')
+            return redirect('home')
         else:
             messages.success(request,
                              "There was an error logging in, please try again")
