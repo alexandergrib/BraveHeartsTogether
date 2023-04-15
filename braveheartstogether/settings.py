f220d118684cc0376e32f93608762afe19097d8e
Aiga <aiga.andrejeva@gmail.com>
2023-04-15 15:55:00 +0000

    cards updated

--- braveheartstogether/settings.py
+++ braveheartstogether/settings.py
@@ -150,7 +150,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
 STATIC_URL = f'https://cdn.gradko.ovh/braveheart/staticfiles/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
